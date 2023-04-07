4a3fc1f61b15010545609621f9072917de58ea5e
tquangdang <thanhquangdang2002@gmail.com>
2023-04-06 20:51:14 -0500

    Change back to HTML

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_ghw():
-    return "Hello, me may beo!"
+    return "<p>Hello, me may beo!</p>"
 
 if __name__ == "__main__":
     app.run(debug = True)
