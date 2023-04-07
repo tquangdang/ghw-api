da822d2c6c8ed859b85b0b5befa66563b45d7f1b
tquangdang <thanhquangdang2002@gmail.com>
2023-04-06 20:26:54 -0500

    Adding HTML

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_ghw():
-    return "Hello, API Hackers!"
+    return "<p>Hello, API Hackers!</p>"
 
 if __name__ == "__main__":
     app.run(debug = True)
