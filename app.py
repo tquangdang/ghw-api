60fb6f738861f999bb49638e8e0d6635c17715a7
tquangdang <thanhquangdang2002@gmail.com>
2023-04-06 20:43:57 -0500

    Delete <p> to see what happen

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_ghw():
-    return "<p>Hello, API Hackers!</p>"
+    return "Hello, API Hackers!"
 
 if __name__ == "__main__":
     app.run(debug = True)
